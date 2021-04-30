e1491b980df69ab5aa4a6df3353f8d7c6d884515
emmanuelnc <emmanuelnc@gmail.com>
2021-04-29 21:00:59 -0500

    Se llega hasta el login

--- pages/pedidos.js
+++ pages/pedidos.js
@@ -2,15 +2,15 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Layout from '../components/Layout'
-const Index = () => (
+const Pedidos = () => (
   <div>
     <Layout>
-      <h1 className="text-2xl text-gray-800 fornt-light">
-        Clientes
+    <h1 className="text-2xl text-gray-800 fornt-light">
+        Pedidos
       </h1>
     </Layout>
 
   </div>
 
 )
-export default Index;
+export default Pedidos;
